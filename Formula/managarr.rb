a0475ba2a1c9f9e9c604ddb184d6d1359343f049
Dark-Alex-17 <alex.j.tusa@gmail.com>
2024-12-20 04:19:29 +0000

    Update formula for Managarr release 0.4.2

--- Formula/managarr.rb
+++ Formula/managarr.rb
@@ -4,16 +4,16 @@ class Managarr < Formula
   desc "A fast and simple dashboard for Kubernetes written in Rust"
   homepage "https://github.com/Dark-Alex-17/managarr"
   if OS.mac? and Hardware::CPU.arm?
-    url "https://github.com/Dark-Alex-17/managarr/releases/download/v0.4.1/managarr-macos-arm64.tar.gz"
-    sha256 "754623934d5f6ffb631f3cc41c4aed29ce6a6c323f7f4022befab806ff4cbe4c"
+    url "https://github.com/Dark-Alex-17/managarr/releases/download/v0.4.2/managarr-macos-arm64.tar.gz"
+    sha256 "0281fcca4aa5ac8693b24abc5373a5ced584bb13c353e49834857b1d844eb34f"
   elsif OS.mac? and Hardware::CPU.intel?
-    url "https://github.com/Dark-Alex-17/managarr/releases/download/v0.4.1/managarr-macos.tar.gz"
-    sha256 "ca55feb77e2bc0e03a2344e75c67fbfa14b7d3dc8e80a0ccfb1e0fea263ce4ed"
+    url "https://github.com/Dark-Alex-17/managarr/releases/download/v0.4.2/managarr-macos.tar.gz"
+    sha256 "8876ee3950d259c6716167fc9d2b52199e6becba2584176b7c0572c48f784b22"
   else
-    url "https://github.com/Dark-Alex-17/managarr/releases/download/v0.4.1/managarr-linux-musl.tar.gz"
-    sha256 "45cf1e06daf56bfc055876b51bd837716b2ea788898b5accc2f6847af4275011"
+    url "https://github.com/Dark-Alex-17/managarr/releases/download/v0.4.2/managarr-linux-musl.tar.gz"
+    sha256 "ab0d9a5368de8f3ad4d26a7504907f17cdc5166e0e48e1763a317d0160d95876"
   end
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   def install
